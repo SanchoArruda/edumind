4d4218e05477cc89fd7d29de1023a8a7274435e7
SanchoArruda <gucairesarruda@gmail.com>
2026-05-12 09:24:16 -0300

    fix: configura envio de email com Brevo

--- config/settings.py
+++ config/settings.py
@@ -159,7 +159,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # E-mail
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.smtp.EmailBackend"
+)
 
 EMAIL_HOST = os.getenv("EMAIL_HOST", "")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
